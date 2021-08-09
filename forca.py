41580df368175536597b57fd7af7dc05262af473
eduardohss <eduhs.souza@gmail.com>
2021-08-09 09:42:48 -0300

    Initial commit

---
+++ forca.py
@@ -0,0 +1,10 @@
+def jogar():
+    print("********************************")
+    print("Bem-vindo ao jogo de Forca")
+    print("********************************")
+
+    print("Fim de jogo")
+
+
+if __name__ == "__main__":
+    jogar()
